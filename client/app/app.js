6ec56115b90d60b6163bd04c426a38c591380fb5
Nequil Hill <ccoman0201@gmail.com>
2017-08-28 16:24:53 -0500

    Routes, single page, and List page started

--- client/app/app.js
+++ client/app/app.js
@@ -1,28 +1,62 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOMCONTENTLOADED')
-})
-var Fitness = angular.module("Fitness", ['ngRoute'])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when("/home", {
-                templateUrl: ""
-            })
-            .when("/StrengthTraining", {
-                templateUrl: ""
-            })
-            .when("/Many/:type", {
-                templateUrl: ""
-            })
-            .when("/All", {
-                templateUrl: ""
-            })
-            .when("/About", {
-                templateUrl: ""
-            })
-            .when("/one/:id", {
-                templateUrl: ""
-            });
-    })
+var Fitness = angular.module("Fitness" ['ngRoute'])
 
-    
-    
+Fitness.config(function ($routeProvider) {
+    $routeProvider
+        .when("/home", {
+            templateUrl: "../views/home.html"
+        })
+        .when("/coach/:type", {
+            templateUrl: "../views/list.html"
+        })
+        .when("/coach", {
+            templateUrl: "../views/list.html"
+        })
+        .when("/About", {
+            templateUrl: "../views/about.html"
+        })
+        .when("/coach/:id", {
+            templateUrl: "../views/single.html"
+        })
+});
+
+Fitness.controller('listCtrl', function ($http, $scope, $routeParams, $location) {
+    var id = $routeParams.id
+    $http.get('http://localhost:3000/api/coach', {
+    })
+        .then(function (success) {
+            $scope.data = success.data
+            console.log(success.data)
+        }, function (err) {
+            alert('something went wrong')
+        })
+    $scope.getId = function (id) {
+        $location.path('/coach/' + id)
+    }
+});
+
+Fitness.controller('singleCtrl', function($http, $scope, $location){
+    var id = $routeParams.id
+    http.get('http://localhost:3000/api/coach'+id,{
+    })
+        .then(function(success) {
+            $scope.data=success.data
+            HTMLFormControlsCollection.log(success.data)
+        }, function (err){
+            alert('something else went wrong')
+        })
+});
+
+Fitness.controller('typeCtrl', function ($http, $scope, $routeParams, $location) {
+    var type = $routeParams.type
+    $http.get('http://localhost:3000/api/coach' + type, {
+    })
+        .then(function (success) {
+            $scope.data = success.data
+            console.log(success.data)
+        }, function (err) {
+            alert('something went wrong')
+        })
+    $scope.getId = function (id) {
+        $location.path('/coach/' + id)
+    }
+});
