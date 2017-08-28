a4c9102567f5ff3af2481ec761db4c7548a82596
Nequil Hill <nequil17@hotmail.com>
2017-08-28 11:34:00 -0500

    Routing start and index

--- client/app/app.js
+++ client/app/app.js
@@ -1,2 +1,26 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMCONTENTLOADED')
+})
+var Fitness = angular.module("Fitness", ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: ""
+            })
+            .when("/StrengthTraining", {
+                templateUrl: ""
+            })
+            .when("/Many/:type", {
+                templateUrl: ""
+            })
+            .when("/All", {
+                templateUrl: ""
+            })
+            .when("/About", {
+                templateUrl: ""
+            })
+            .when("/one/:id", {
+                templateUrl: ""
+            });
+    })
     
