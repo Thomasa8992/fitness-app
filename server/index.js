c0a22f372fa971ddb76ab3a26461b12204614fd0
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-08-29 09:41:27 -0500

    added ability to grab type from server

--- server/index.js
+++ server/index.js
@@ -36,7 +36,51 @@ app.route('/api/coach/:id')
             }
         });
     })
-    
+app.route('/api/coach/:type')
+    .get(function(req, res) {
+        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
+            if (err) {
+                res.statusStatus(500);
+            } else {
+                var chunks = JSON.parse(fileContents);
+                var type = req.params.type;
+                var response;
+                chunks.forEach(function(chunk) {
+                    if (chunk.type === type) {
+                        response = chunk;
+                    }
+                });
+                if (response) {
+                    res.send(response);
+                } else {
+                    res.sendStatus(404);
+                }
+            }
+        });
+    })
+app.route("/api/category/:type")
+    .get(function(req, res){
+        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
+            if (err) {
+                res.statusStatus(500);
+            } else {
+                var chunks = JSON.parse(fileContents);
+                var type = req.params.type;
+                var response = chunks.filter(function(chunk) {
+                    if(chunk.type){
+                        if (chunk.type.toLowerCase().trim() === type.toLowerCase().trim()) {
+                            return chunk
+                        }
+                    }
+                });
+                if (response) {
+                    res.send(response);
+                } else {
+                    res.sendStatus(404);
+                }
+            }
+        });
+    })
 app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
