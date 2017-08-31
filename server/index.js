7df7baa6c7b42ca88b61752ec853b05dfe03e13c
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-08-30 19:40:35 -0500

    type works from firebase now

--- server/index.js
+++ server/index.js
@@ -119,17 +119,50 @@ app.route('/api/coach/:id')
         });
     })
 app.route("/api/category/:type")
-    .get(function(req, res){
-        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
-            if (err) {
-                res.statusStatus(500);
-            } else {
-                var chunks = JSON.parse(fileContents);
-                var type = req.params.type;
-                var response = chunks.filter(function(chunk) {
-                    if(chunk.type){
-                        if (chunk.type.toLowerCase().trim() === type.toLowerCase().trim()) {
-                            return chunk
+    // .get(function(req, res){
+    //     fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
+    //         if (err) {
+    //             res.statusStatus(500);
+    //         } else {
+    //             var chunks = JSON.parse(fileContents);
+    //             var type = req.params.type;
+    //             var response = chunks.filter(function(chunk) {
+    //                 if(chunk.type){
+    //                     if (chunk.type.toLowerCase().trim() === type.toLowerCase().trim()) {
+    //                         return chunk
+    //                     }
+    //                 }
+    //             });
+    //             if (response) {
+    //                 res.send(response);
+    //             } else {
+    //                 res.sendStatus(404);
+    //             }
+    //         }
+    //     });
+    // })
+    .get(function(req, res, next) {
+    console.log('in fb route');
+
+    var config = {
+        apiKey: 'AIzaSyC0aUJENU5pDKGN1Sf9wIZeID2449QJS-c',
+        authDomain: 'innovate-fitness-app.firebaseapp.com',
+        databaseURL: 'https://innovate-fitness-app.firebaseio.com/',
+        storageBucket: 'gs://innovate-fitness-app.appspot.com'
+    };
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+    // var id = req.params.id
+    firebase.database().ref('/fitness-app').once('value')
+    .then((success) => {
+        var values = success.val();
+        var type = req.params.type;
+            var response = values.filter(function(value) {
+                    if(value.type){
+                        if (value.type.toLowerCase().trim() === type.toLowerCase().trim()) {
+                            return value
                         }
                     }
                 });
@@ -138,40 +171,8 @@ app.route("/api/category/:type")
                 } else {
                     res.sendStatus(404);
                 }
-            }
+            });
         });
-    })
-    // .get(function(req, res, next) {
-    // console.log('in fb route');
-
-    // var config = {
-    //     apiKey: 'AIzaSyC0aUJENU5pDKGN1Sf9wIZeID2449QJS-c',
-    //     authDomain: 'innovate-fitness-app.firebaseapp.com',
-    //     databaseURL: 'https://innovate-fitness-app.firebaseio.com/',
-    //     storageBucket: 'gs://innovate-fitness-app.appspot.com'
-    // };
-
-    // if (!firebase.apps.length) {
-    //     firebase.initializeApp(config);
-    // }
-    // // var id = req.params.id
-    // firebase.database().ref('/fitness-app').once('value')
-    // .then((success) => {
-    //     var values = success.val();
-    //     var type = req.params.type;
-    //         var response;
-    //         values.forEach(function(value) { 
-    //             if (value.type === type) {
-    //                 response = value;
-    //             }    
-    //         });
-    //         if (response) {
-    //                 res.send(response);
-    //             } else {
-    //                 res.sendStatus(404);
-    //             }
-    //     });
-    // })
 app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
