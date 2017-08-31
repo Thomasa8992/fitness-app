e92959cae24ed468ae9fe7681972e694d1d46fb1
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-08-30 19:34:34 -0500

    single page works with firebase

--- server/index.js
+++ server/index.js
@@ -7,14 +7,12 @@ var firebase = require('firebase');
 var clientPath = path.join(__dirname, '..', 'client');
 var jsonPath = path.join(__dirname, 'data.json');
 app.use(express.static(clientPath));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.route('/api/coach')
-//     .get(function(req, res) {
-//         res.sendFile(jsonPath);
-//     })
+
 
 .get(function(res, res, next) {
     console.log('in fb route');
@@ -34,13 +32,6 @@ app.route('/api/coach')
     .then((success) => {
         res.send(success.val());
     });
-    // var getAll = firebase.database().ref('/fitness-app')
-    // getAll.on('value',function(getInfo){
-    //     res.send(getInfo.val());
-    // })
-    // .then((success) => {
-        
-    // });
 });
 
 app.route('/api/post')
@@ -96,47 +87,35 @@ app.route('/api/post')
         }
     })*/
 app.route('/api/coach/:id')
-    .get(function(req, res) {
-        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
-            if (err) {
-                res.statusStatus(500);
-            } else {
-                var chunks = JSON.parse(fileContents);
-                var id = req.params.id;
-                var response;
-                chunks.forEach(function(chunk) {
-                    if (chunk.id === id) {
-                        response = chunk;
-                    }
-                });
-                if (response) {
+    .get(function(req, res, next) {
+    console.log('in fb route');
+
+    var config = {
+        apiKey: 'AIzaSyC0aUJENU5pDKGN1Sf9wIZeID2449QJS-c',
+        authDomain: 'innovate-fitness-app.firebaseapp.com',
+        databaseURL: 'https://innovate-fitness-app.firebaseio.com/',
+        storageBucket: 'gs://innovate-fitness-app.appspot.com'
+    };
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+    // var id = req.params.id
+    firebase.database().ref('/fitness-app').once('value')
+    .then((success) => {
+        var values = success.val();
+        var id = req.params.id;
+            var response;
+            values.forEach(function(value) {
+                if (value.id === id) {
+                    response = value;
+                }    
+            });
+            if (response) {
                     res.send(response);
                 } else {
                     res.sendStatus(404);
                 }
-            }
-        });
-    })
-app.route('/api/coach/:type')
-    .get(function(req, res) {
-        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
-            if (err) {
-                res.statusStatus(500);
-            } else {
-                var chunks = JSON.parse(fileContents);
-                var type = req.params.type;
-                var response;
-                chunks.forEach(function(chunk) {
-                    if (chunk.type === type) {
-                        response = chunk;
-                    }
-                });
-                if (response) {
-                    res.send(response);
-                } else {
-                    res.sendStatus(404);
-                }
-            }
         });
     })
 app.route("/api/category/:type")
@@ -162,6 +141,37 @@ app.route("/api/category/:type")
             }
         });
     })
+    // .get(function(req, res, next) {
+    // console.log('in fb route');
+
+    // var config = {
+    //     apiKey: 'AIzaSyC0aUJENU5pDKGN1Sf9wIZeID2449QJS-c',
+    //     authDomain: 'innovate-fitness-app.firebaseapp.com',
+    //     databaseURL: 'https://innovate-fitness-app.firebaseio.com/',
+    //     storageBucket: 'gs://innovate-fitness-app.appspot.com'
+    // };
+
+    // if (!firebase.apps.length) {
+    //     firebase.initializeApp(config);
+    // }
+    // // var id = req.params.id
+    // firebase.database().ref('/fitness-app').once('value')
+    // .then((success) => {
+    //     var values = success.val();
+    //     var type = req.params.type;
+    //         var response;
+    //         values.forEach(function(value) { 
+    //             if (value.type === type) {
+    //                 response = value;
+    //             }    
+    //         });
+    //         if (response) {
+    //                 res.send(response);
+    //             } else {
+    //                 res.sendStatus(404);
+    //             }
+    //     });
+    // })
 app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
